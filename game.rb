57e2e9cb5e564b8fa44616c527001480a1a32866
cmkoller <cmkoller01@gmail.com>
2015-11-09 13:46:03 -0500

    First commit

---
+++ game.rb
@@ -0,0 +1,14 @@
+require_relative 'player'
+
+class Game
+  def initialize
+    @players = []
+  end
+
+  # def add_player(new_player)
+
+  # def play_game
+    # - Each round, randomly assign one player a point.
+    # - When one player reaches five points, they win!
+
+end
