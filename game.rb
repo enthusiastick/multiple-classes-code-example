86d9c83f05e1dd88d52cd60f45daad420612615a
cmkoller <cmkoller01@gmail.com>
2015-11-10 11:53:25 -0500

    Implement game and player classes

--- game.rb
+++ game.rb
@@ -1,14 +1,39 @@
+require 'pry'
 require_relative 'player'
 
 class Game
+  attr_reader :players
+
   def initialize
     @players = []
   end
 
-  # def add_player(player_name)
+  def add_player(player_name)
+    @players << Player.new(player_name)
+  end
 
-  # def play_game
-    # - Each round, randomly assign one player a point.
-    # - When one player reaches five points, they win!
+  def play_game
+    while winning_player.points < 5
+      assign_random_point
+    end
 
+    puts "#{winning_player.name} wins the game!"
+  end
+
+  private
+
+  def winning_player
+    @players.max_by { |player| player.points }
+  end
+
+  def assign_random_point
+    lucky_player = @players.sample
+    puts "#{lucky_player.name} is the luck player!"
+    lucky_player.points += 1
+    puts "#{lucky_player.name} now has #{lucky_player.points} points!"
+  end
 end
+
+g = Game.new
+g.add_player("Justin")
+g.play_game
